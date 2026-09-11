ffb3ccd740241ce58c35cf7a95b6a84c2d3cfbde
Emaury <ONYENEHOE0912@student.bowiestate.edu>
2026-09-11 05:39:36 -0400

    Complete Lab Assignment 2

--- src/SimpleInputOutput.java
+++ src/SimpleInputOutput.java
@@ -4,6 +4,7 @@
  */
 
 //import classes
+import java.util.Scanner;
 
 /**
  *
@@ -13,9 +14,48 @@ public class SimpleInputOutput {
 
     public static void main(String[] args) {
         //variable declaration
-        
+        Scanner console = new Scanner(System.in);
+
+        final int SECRET = 11;
+        final double RATE = 12.50;
+
+        int num1;
+        int num2;
+        int newNum;
+        String name;
+        double hoursWorked;
+        double wages;
+
         //executable statements
-        
+        System.out.print("Enter the first integer: ");
+        num1 = console.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        num2 = console.nextInt();
+
+        System.out.println("The value of num1 = " + num1
+                + " and the value of num2 = " + num2 + ".");
+
+        newNum = (num1 * 2) + num2;
+
+        System.out.println("The value of newNum = " + newNum);
+
+        newNum = newNum + SECRET;
+
+        System.out.println("After adding SECRET, newNum = " + newNum);
+
+        System.out.print("Enter the last name: ");
+        name = console.next();
+
+        System.out.print("Enter hours worked: ");
+        hoursWorked = console.nextDouble();
+
+        wages = RATE * hoursWorked;
+
+        System.out.println("Name: " + name);
+        System.out.println("Pay Rate: $" + RATE);
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Salary: $" + wages);
     }
 
 }
